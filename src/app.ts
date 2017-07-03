ba848d75331d9077a6090c4997e08a05da52aa73
Bryan Freeman <bryanfree66@gmail.com>
2017-07-03 16:12:02 -0700

    Site starting

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import {NgModule, Component} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 @Component({
     selector: 'nomad-web-app',
