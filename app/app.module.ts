c2672ef9a2b9485eca8654a5a8f0bb77f4a130fb
Bryan Freeman <bryanfree66@gmail.com>
2017-07-07 16:12:18 -0700

    application updates

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }  from './components/application/app.component';
+import { HeaderComponent }  from './components/header/header.component';
 import { MaterialModule} from '@angular/material';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
         BrowserModule,
         MaterialModule
     ],
-    declarations: [ AppComponent ],
+    declarations: [ AppComponent, HeaderComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
--- app/components/header/header.component.ts
+++ app/components/header/header.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-    selector: 'nomad-application',
+    selector: 'nomad-header',
     templateUrl: './app/components/header/header.html',
 
 })
