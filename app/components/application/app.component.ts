b2c3f34bafef5363e5e811e627efd6cfbf4e8056
Bryan Freeman <bryanfree66@gmail.com>
2017-07-07 17:07:59 -0700

    application page layout

--- app/components/application/app.component.ts
+++ app/components/application/app.component.ts
@@ -4,4 +4,21 @@ import {Component} from '@angular/core';
     selector: 'nomad-application',
     templateUrl: './app/components/application/application.html'
 })
-export class AppComponent {}
+export class AppComponent {
+    innerHeight: any;
+    innerWidth: any;
+
+    constructor(){
+        // 75% screen height
+        this.innerHeight = (window.screen.height * .75) + "px";
+        this.innerWidth = (window.screen.width * .986) + "px";
+    }
+
+    getHeight(){
+        return this.innerHeight;
+    }
+
+    getWidth(){
+        return this.innerWidth;
+    }
+}
