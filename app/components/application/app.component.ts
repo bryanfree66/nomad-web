525a2d4594dbfb0cd8416e4663550ac6505883c9
Bryan Freeman <bryanfree66@gmail.com>
2017-07-07 19:45:46 -0700

    updated app and css

--- app/components/application/app.component.ts
+++ app/components/application/app.component.ts
@@ -4,21 +4,4 @@ import {Component} from '@angular/core';
     selector: 'nomad-application',
     templateUrl: './app/components/application/application.html'
 })
-export class AppComponent {
-    innerHeight: any;
-    innerWidth: any;
-
-    constructor(){
-        // 75% screen height
-        this.innerHeight = (window.innerHeight * .75) + "px";
-        this.innerWidth = (window.innerWidth * .986) + "px";
-    }
-
-    getHeight(){
-        return this.innerHeight;
-    }
-
-    getWidth(){
-        return this.innerWidth;
-    }
-}
+export class AppComponent {}
--- app/components/header/header.component.ts
+++ app/components/header/header.component.ts
@@ -3,6 +3,6 @@ import {Component} from '@angular/core';
 @Component({
     selector: 'nomad-header',
     templateUrl: './app/components/header/header.html',
-
+    styleUrls: ['styles/styles.css']
 })
 export class HeaderComponent {}
